1fd6e2a60dd134e1bd096254cc53b75f2887d128
kanoi <gitkanoi@k1k2.com>
2014-08-07 04:32:28 +1000

    tables.sql simple all table row count

---
+++ sql/tables.sql
@@ -0,0 +1,23 @@
+\t
+select 'accountadjustment' as "accountadjustment",count(*) from accountadjustment;
+select 'accountbalance' as "accountbalance",count(*) from accountbalance;
+select 'auths' as "auths",count(*) from auths;
+select 'blocks' as "blocks",count(*) from blocks;
+select 'eventlog' as "eventlog",count(*) from eventlog;
+select 'idcontrol' as "idcontrol",count(*) from idcontrol;
+select 'markersummary' as "markersummary",count(*) from markersummary;
+select 'miningpayouts' as "miningpayouts",count(*) from miningpayouts;
+select 'optioncontrol' as "optioncontrol",count(*) from optioncontrol;
+select 'paymentaddresses' as "paymentaddresses",count(*) from paymentaddresses;
+select 'payments' as "payments",count(*) from payments;
+select 'poolstats' as "poolstats",count(*) from poolstats;
+select 'shareerrors' as "shareerrors",count(*) from shareerrors;
+select 'shares' as "shares",count(*) from shares;
+select 'sharesummary' as "sharesummary",count(*) from sharesummary;
+select 'users' as "users",count(*) from users;
+select 'userstats' as "userstats",count(*) from userstats;
+select 'version' as "version",count(*) from version;
+select 'workers' as "workers",count(*) from workers;
+select 'workinfo' as "workinfo",count(*) from workinfo;
+select 'workmarkers' as "workmarkers",count(*) from workmarkers;
+\t
