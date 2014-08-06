7268672485dc5ea325a5ec76d4254b6114956b31
kanoi <gitkanoi@k1k2.com>
2014-08-06 19:07:38 +1000

    sql - reloadstatus.sql - show what the db reload status is

---
+++ sql/reloadstatus.sql
@@ -0,0 +1,5 @@
+select 'sharesummary' as "sharesummary",min(firstshare) as "min incomplete firstshare" from sharesummary where complete = 'n';
+select 'workinfo' as "workinfo",max(createdate) as "max createdate" from workinfo;
+select 'auths' as "auths",max(createdate) as "max createdate" from auths;
+select 'poolstats' as "poolstats",max(createdate) as "max createdate" from poolstats;
+select 'userstats' as "userstats",max(statsdate) as "max statsdate - start of this hour" ,userid,workername from userstats group by userid,workername;
