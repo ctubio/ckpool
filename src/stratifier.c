03ba50a3e294a15ca2f53a585b9c8b56db88d7f0
Con Kolivas <kernel@kolivas.org>
2014-08-21 12:14:28 +1000

    Workbase_id should be updated even in proxy mode or diff will only change once

--- src/stratifier.c
+++ src/stratifier.c
@@ -522,8 +522,7 @@ static void add_base(ckpool_t *ckp, workbase_t *wb, bool *new_block)
 	wb->logdir = ckalloc(len);
 
 	ck_wlock(&workbase_lock);
-	if (!ckp->proxy)
-		wb->id = workbase_id++;
+	wb->id = workbase_id++;
 
 	if (strncmp(wb->prevhash, lasthash, 64)) {
 		*new_block = true;
