988863f9c58c74a440b1d7211f331565701e99b0
kanoi <gitkanoi@k1k2.com>
2014-10-10 13:41:50 +1100

    php - add a worker.php for address miners

---
+++ pool/worker.php
@@ -0,0 +1,21 @@
+<?php
+usleep(100000);
+include_once('param.php');
+?>
+<!DOCTYPE html>
+<html><head><title>CKPool</title><meta content='text/html; charset=iso-8859-1' http-equiv='Content-Type'></head><body>
+<?php
+function go()
+{
+ $a = getparam('a', true);
+ if (substr($a, 0, 1) != '1')
+	return;
+ if (preg_match('/^[a-zA-Z0-9]{24,}[\._]?[a-zA-Z0-9\._]*$/', $a) === false)
+	return;
+ $sta = "../pool/workers/$a";
+ if (file_exists($sta))
+	echo file_get_contents($sta);
+}
+go();
+?>
+</body></html>
