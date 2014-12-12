6ae7b58bb042a38cfa58f079ee26cfa390eb2c23
kanoi <gitkanoi@k1k2.com>
2014-12-12 15:31:02 +1100

    php - update payout description to match kano pool

--- pool/page_payout.php
+++ pool/page_payout.php
@@ -5,10 +5,16 @@ function dopayout($data, $user)
  $pg = '<h1>Payouts</h1>';
  $pg .= '<table width=75% cellpadding=0 cellspacing=0 border=0>';
  $pg .= '<tr><td class=dc>';
- $pg .= 'We use PPLNS (pay per last N shares)<br>';
- $pg .= 'Pool fee is 1.5%<br>';
+ $pg .= 'We use PPLNS (pay per last N shares)<br><br>';
  $pg .= 'The N value used for PPLNS is the network difficulty';
- $pg .= ' when a block is found.';
+ $pg .= ' when a block is found,<br>';
+ $pg .= 'but includes the full shift at the start and end of the range,<br>';
+ $pg .= 'so it usually will be a bit more than N.<br><br>';
+ $pg .= 'Shifts are ~30s long, however, when a block is found<br>';
+ $pg .= 'the current shift ends at the point the block was found.<br>';
+ $pg .= 'A ckpool restart will also start a new shift.<br><br>';
+ $pg .= 'Transaction fees are included in the miner payout.<br>';
+ $pg .= 'Pool fee is 0.9% of the total.<br>';
  $pg .= '</td></tr></table>';
  return $pg;
 }
