9a7e4c4b3f17bd4e18cbd528fc28a16f000d2242
kanoi <gitkanoi@k1k2.com>
2014-12-30 10:35:56 +1100

    php - suggest to not break in the user 'Hash Rate:' text

--- pool/page.php
+++ pool/page.php
@@ -340,7 +340,7 @@ function pgtop($info, $dotop, $user, $douser)
 			}
 			$top .= "
 <span class=topwho>".htmlspecialchars($who)."$extra&nbsp;</span><br>
-<span class=topdes>Hash Rate:</span>
+<span class=topdes>Hash&nbsp;Rate:</span>
 <span class=topdat>$uhr$u1hr</span><br>";
 			$top .= makeForm('')."
 &nbsp;<input type=submit name=Logout value=Logout>
