ef805811d2b5ae9d51d20c9cbae29064481a04c7
ckolivas <kernel@kolivas.org>
2015-01-19 15:14:03 +1100

    Update copyright years on affected files

--- src/jansson-2.6/src/strbuffer.c
+++ src/jansson-2.6/src/strbuffer.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009-2013 Petri Lehtinen <petri@digip.org>
+ * Copyright (c) 2015 Con Kolivas <kernel@kolivas.org>
  *
  * Jansson is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
