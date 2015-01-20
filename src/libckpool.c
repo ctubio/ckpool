827f42ba739a8aee3519e8657307558f417cb91f
Con Kolivas <kernel@kolivas.org>
2015-01-21 02:15:13 +1100

    Fix impossible >= UNIX_PATH_MAX warning

--- src/libckpool.c
+++ src/libckpool.c
@@ -793,7 +793,7 @@ int _open_unix_client(const char *server_path, const char *file, const char *fun
 
 	if (likely(server_path)) {
 		len = strlen(server_path);
-		if (unlikely(len < 1 || len > UNIX_PATH_MAX)) {
+		if (unlikely(len < 1 || len >= UNIX_PATH_MAX)) {
 			LOGERR("Invalid server path length %d in open_unix_client", len);
 			goto out;
 		}
