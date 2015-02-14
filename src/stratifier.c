aa153ba7425acf66b0e02f3c5fa786b2f2f1d53e
Con Kolivas <kernel@kolivas.org>
2015-02-14 11:25:33 +1100

    Differentiate pong from other spurious messages from clients

--- src/stratifier.c
+++ src/stratifier.c
@@ -3691,7 +3691,11 @@ static void parse_instance_msg(ckpool_t *ckp, sdata_t *sdata, smsg_t *msg, strat
 		if (res_val) {
 			const char *result = json_string_value(res_val);
 
-			LOGDEBUG("Received spurious response %s", result ? result : "");
+			if (!safecmp(result, "pong"))
+				LOGDEBUG("Received pong from client %"PRId64, client_id);
+			else
+				LOGDEBUG("Received spurious response %s from client %"PRId64,
+					 result ? result : "", client_id);
 			goto out;
 		}
 		send_json_err(sdata, client_id, id_val, "-3:method not found");
