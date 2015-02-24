ac86d9b736bc4293959770e0d81bfb881c6562fb
Con Kolivas <kernel@kolivas.org>
2015-02-24 12:43:10 +1100

    Add an HASH_REPLACE_I64 helper macro

--- src/uthash.h
+++ src/uthash.h
@@ -265,6 +265,8 @@ do {                                                                            
     HASH_FIND(hh,head,findint,sizeof(int64_t),out)
 #define HASH_ADD_I64(head,intfield,add)                                          \
     HASH_ADD(hh,head,intfield,sizeof(int64_t),add)
+#define HASH_REPLACE_I64(head,intfield,add,replaced)                             \
+    HASH_REPLACE(hh,head,intfield,sizeof(int64_t),add,replaced)
 #define HASH_FIND_PTR(head,findptr,out)                                          \
     HASH_FIND(hh,head,findptr,sizeof(void *),out)
 #define HASH_ADD_PTR(head,ptrfield,add)                                          \
