89935e876cc1e3a2d73743811f4c505e6e0cabcf
kanoi <gitkanoi@k1k2.com>
2015-03-21 01:37:15 +1100

    php - store graph settings in a cookie

--- pool/page.php
+++ pool/page.php
@@ -34,10 +34,77 @@ function addScript($script)
  if ($script != null and trim($script) != '')
  {
 	if ($page_scripts == '')
-		$page_scripts = "<script type='text/javascript'><!--\n";
+		$page_scripts = "<script type='text/javascript'>\n";
 
 	$page_scripts .= trim($script);
  }
+}
+#
+function addGBase()
+{
+ $g = "function hasCan(){var c0=document.getElementById('can0');c=document.getElementById('can');return !!(c0&&c&&c.getContext&&c.getContext('2d'));}
+function sep(d){ans={};var ar=d.split('\\t');var l=ar.length;for(var i=0;i<l;i++){var e=ar[i].indexOf('=');ans[ar[i].substr(0,e)]=ar[i].substr(e+1)};return ans}
+function dfmt(c,e){var d=new Date(e*1000);var DD,HH,MM;if(c['utc']){DD=d.getUTCDate();HH=d.getUTCHours();MM=d.getUTCMinutes()}else{DD=d.getDate();HH=d.getHours();MM=d.getMinutes()}var ans=''+DD+'/';if(HH<10){ans+='0'}ans+=''+HH+':';if(MM<10){ans+='0'}ans+=''+MM;return ans}
+function gcn(n){var ans='',d=document.cookie;if(d){var c0=d.indexOf(n+'='),cs=d.indexOf(' '+n+'=');if(c0==0||cs>0){if(cs>0){c0=cs+1}var c=d.substr(c0).split(';',1);var e=c[0].indexOf('=');if(e>0){ans=c[0].substr(e+1)}}}return ans}
+function scnv(n,v){var d = new Date();d.setTime(d.getTime()+(864*Math.pow(10,8)));document.cookie=n+'='+v+'; expires='+d.toUTCString()+'; path=/'}
+function ccb(c,n){var e=document.getElementById(n);c[n]=(e&&e.checked)}
+function gch(z,zm){if(z<0.5){return 0.5}if(z>(zm-0.5)){return(zm-0.5)}return z}
+function gchx(c,x){return gch(x*c['xm']+c['xo'],c['ctx'].canvas.width)}
+function gchy(c,y){return gch((1-y)*c['ym']+c['yo'],c['ctx'].canvas.height)}
+function gx0(c){return -c['xo']/c['xm']};
+function gy0(c){return -c['yo']/c['ym']};
+function gto(c,xo,yo){c['xo']+=xo;c['yo']+=yo}
+function gts(c,xs,ys){c['xm']*=xs;c['ym']*=ys}
+function gtso(c,xs,ys){gto(c,c['xm']*(1.0-xs)/2.0,c['ym']*(1.0-ys)/2.0);gts(c,xs,ys)}
+function gfs(c,bg){c['ctx'].fillStyle=bg}
+function gss(c,fg){c['ctx'].strokeStyle=fg}
+function glw(c,w){c['ctx'].lineWidth=w}
+function gfz(c,x,y,ox,oy,t,co,a){gfs(c,co);c['ctx'].textAlign=a;c['ctx'].fillText(t,gchx(c,x)+ox,gchy(c,y)-oy)}
+function gbe(c,x,y){c['ctx'].beginPath();c['ctx'].moveTo(gchx(c,x),gchy(c,y))}
+function gln(c,x,y){c['ctx'].lineTo(gchx(c,x),gchy(c,y))}
+function gct(c,x1,y1,x2,y2,x3,y3){c['ctx'].bezierCurveTo(gchx(c,x1),gchy(c,y1),gchx(c,x2),gchy(c,y2),gchx(c,x3),gchy(c,y3))}
+function glm(c,x,y){c['ctx'].moveTo(gchx(c,x),gchy(c,y))}
+function gle(c){c['ctx'].closePath()}
+function gfl(c){c['ctx'].fill()}
+function gst(c){c['ctx'].stroke()}
+function gfi(c){gle(c);gst(c)}
+function gbd(c){gbe(c,0,0);gln(c,1,0);gln(c,1,1);gln(c,0,1);gle(c);gfl(c);gst(c)}
+function ggr(c,xs,ys,yt,xn,x0,x1,y0,y1,ar,nx,vx,vy,av){
+gtso(c,xs,ys);
+gss(c,'black');glw(c,1.5);
+gbe(c,0,1);gln(c,0,0);gln(c,1,0);gst(c);
+glw(c,0.2);
+var hi=c['ctx'].measureText('M').width, wi=c['ctx'].measureText('1').width;
+for(var i=0;i<11;i++){var y=i/10.0;gbe(c,-0.01,y);gln(c,1,y);gst(c);var t=''+(((y1-y0)*i/10+y0).toFixed(2));gfz(c,0,y,-wi,0,t,'black','end')}
+gfz(c,gx0(c),0.55,wi,0,yt,'#0080ff','left');
+var m=Math.round(0.5+xn/20.0);
+var f=1;
+for(var i=0;i<xn;i++){var n=ar[nx+i];var x=ar[vx+i];var xo=(x-x0)/(x1-x0);if(c['skey']&&(i<(xn-1))&&(i%m)==0){gbe(c,xo,0);gln(c,xo,-0.01);gst(c);gfz(c,xo,0,0,-hi*1.5,n,'#00a050','center')}if(c['slines']){gbe(c,xo,0);gln(c,xo,1);gst(c)}}
+var xhr=x1-(x1%3600);
+gss(c,'brown');
+if(c['tkey']||c['tlines']){var hlv=c['hln'][c['hl']];hrs=c['hrs'][c['hr']]*3600/hlv;
+var l=0;tpos=2.7;if(c['over']){tpos=1.5}
+for(var i=xhr;i>=x0;i-=hrs){var n=dfmt(c,i);var xo=(i-x0)/(x1-x0);if(c['tkey']&&((l%hlv)==0)){gbe(c,xo,0);gln(c,xo,-0.02);gst(c);gfz(c,xo,0,0,-hi*tpos,n,'brown','center')}if(c['tlines']){gbe(c,xo,0);gln(c,xo,1);gst(c)}l++}}
+glw(c,1);
+gss(c,'#0000c0');
+if(c['smooth']){var xa=0,ya=0,xb=0,yb=0;
+for(var i=0;i<xn;i++){var x=ar[vx+i];var y=ar[vy+i];var xo=(x-x0)/(x1-x0);var yo=(y-y0)/(y1-y0);if(f==1){gbe(c,xo,yo);f=0;xb=xo;yb=yo}else{gct(c,(xa+xb)/2,(ya+yb)/2,xb,yb,(xb+xo)/2,(yb+yo)/2)}xa=xb;ya=yb;xb=xo;yb=yo}gct(c,(xa+xb)/2,(ya+yb)/2,xo,yo,xo,yo);gst(c);}
+else{for(var i=0;i<xn;i++){var x=ar[vx+i];var y=ar[vy+i];var xo=(x-x0)/(x1-x0);var yo=(y-y0)/(y1-y0);if(f==1){gbe(c,xo,yo);f=0}else{gln(c,xo,yo)}}gst(c);}
+glw(c,1);
+gss(c,'red');
+var y=(av-y0)/(y1-y0);
+gbe(c,0,y);gln(c,1,y);gst(c);
+var t=''+av.toFixed(2)+'av';gfz(c,1,y,1,0,t,'red','left')
+if(c['tkey']){var col,hrl=c['hrs'].length;for(var i=0;i<hrl;i++){if(c['hr']==i){col='red'}else{col='black'}gfz(c,1,0,c['xo']-c['pxe'],hi*(i+1)*2,''+c['hrs'][i],col,'end')}for(var i=0;i<c['hln'].length;i++){if(c['hl']==i){col='red'}else{col='black'}gfz(c,1,0,c['xo']-c['pxe'],hi*(i+2+hrl)*2,''+c['hrs'][i],col,'end')}}
+}
+function sn(i,shi){if(shi.indexOf(' Shift ')<0){return ''+(i%10)}else{return shi.replace(/.* ([a-z])[a-z]*$/,'$1')}}
+function gc(c){var div=document.getElementById('can0');while(div.firstChild){div.removeChild(div.firstChild)}c['can']=document.createElement('canvas');c['can'].id='can';c['wx']=window.innerWidth;c['wy']=window.innerHeight;c['xm']=Math.round(c['wx']*0.9+0.5);c['ym']=Math.round(c['wy']*0.8+0.5);if(c['ym']>c['xm']){c['ym']=c['xm']}c['xo']=0.0;c['yo']=0.0;c['ctx']=c['can'].getContext('2d');c['ctx'].canvas.width=c['xm']+1;c['ctx'].canvas.height=c['ym']+1;div.appendChild(c['can']);c['pxe']=Math.max(Math.round(c['xm']/250),1)}
+function opts(t,i){var e=document.getElementById(i);if(t.checked){e.style.visibility='visible'}else{e.style.visibility='hidden'}}
+function ghrs(c){c['hrs']=[1,2,3,4,6,8,12,24];c['hln']=[1,2,3,4,6]}
+function gopt(c,cbx){for(var i=0;i<cbx.length;i++){ccb(c,cbx[i])}c['hr']=4;c['hl']=0}
+function doinit(cbx,xon){for(var i=0;i<cbx.length;i++){var e=document.getElementById(cbx[i]);if(e){var n=gcn(cbx[i]);if(n==''){if(xon[cbx[i]]){e.checked=true}else{e.checked=false}}else{if(n=='1'){e.checked=true}else{e.checked=false}}}}}
+";
+ addScript($g);
 }
 #
 function makeLink($page, $rest = '')
@@ -555,7 +622,7 @@ function gopage($info, $data, $pagefun, $page, $menu, $name, $user, $ispage = tr
 	$pg = str_replace($dbg_marker, cvtdbg(), $pg);
 
  if ($page_scripts != '')
-	$page_scripts .= "//-->\n</script>";
+	$page_scripts .= "</script>";
 
  $head = str_replace($script_marker, $page_scripts, $head);
 
