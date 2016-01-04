29669ee60fb04cfc45ef6d18b6685dd4bcf88cad
Con Kolivas <kernel@kolivas.org>
2016-01-04 15:37:52 +1100

    Add architecture dependent sha256 assembly support on recent intel platforms, linking libckpool only statically

--- src/sha2.c
+++ src/sha2.c
@@ -85,6 +85,31 @@ uint32_t sha256_k[64] =
 
 /* SHA-256 functions */
 
+#ifdef USE_AVX2
+extern void sha256_rorx(const void *, uint32_t[8], uint64_t);
+
+void sha256_transf(sha256_ctx *ctx, const unsigned char *message,
+                   unsigned int block_nb)
+{
+	sha256_rorx(message, ctx->h, block_nb);
+}
+#elif defined(USE_AVX1)
+extern void sha256_avx(const unsigned char *, uint32_t[8], uint64_t);
+
+void sha256_transf(sha256_ctx *ctx, const unsigned char *message,
+                   unsigned int block_nb)
+{
+	sha256_avx(message, ctx->h, block_nb);
+}
+#elif defined(USE_SSE4)
+extern void sha256_sse4(const unsigned char *, uint32_t[8], uint64_t);
+
+void sha256_transf(sha256_ctx *ctx, const unsigned char *message,
+                   unsigned int block_nb)
+{
+	sha256_sse4(message, ctx->h, block_nb);
+}
+#else
 void sha256_transf(sha256_ctx *ctx, const unsigned char *message,
                    unsigned int block_nb)
 {
@@ -130,7 +155,7 @@ void sha256_transf(sha256_ctx *ctx, const unsigned char *message,
         }
     }
 }
-
+#endif
 void sha256(const unsigned char *message, unsigned int len, unsigned char *digest)
 {
     sha256_ctx ctx;
