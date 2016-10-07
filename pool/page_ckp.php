0fb49e96be77a5e3509323b4a7803aac64163255
kanoi <gitkanoi@k1k2.com>
2016-10-07 16:36:19 +1100

    php - add more sorting options to page_ckp

--- pool/page_ckp.php
+++ pool/page_ckp.php
@@ -32,12 +32,12 @@ function dockp($data, $user)
  $pg .= '<thead><tr class=title>';
  $pg .= "<td class=dl><span class=nb>Name:<$r id=srtname data-sf=s0></span></td>";
  $pg .= '<td class=dr>Initial</td>';
- $pg .= '<td class=dr>Allocated</td>';
+ $pg .= "<td class=dr><span class=nb><$r id=srtalloc data-sf=r2>:Alloc</span></td>";
  $pg .= "<td class=dr><span class=nb><$r id=srtstore data-sf=r3>:In&nbsp;Store</span></td>";
  $pg .= "<td class=dr><span class=nb><$r id=srtram data-sf=r4>:RAM</span></td>";
  $pg .= "<td class=dr><span class=nb><$r id=srtram2 data-sf=r5>:RAM2</span></td>";
- $pg .= '<td class=dr>Cull</td>';
- $pg .= '<td class=dr>Limit</td>';
+ $pg .= "<td class=dr><span class=nb><$r id=srtcull data-sf=r6>:Cull</span></td>";
+ $pg .= "<td class=dr><span class=nb><$r id=srtlim data-sf=r7>:Limit</span></td>";
  $pg .= "</tr></thead>\n";
  if ($ans['STATUS'] == 'ok')
  {
@@ -53,12 +53,12 @@ function dockp($data, $user)
 		$pg .= "<tr class=$row>";
 		$pg .= "<td class=dl data-srt='".$ans['name:'.$i]."'>".$ans['name:'.$i].'</td>';
 		$pg .= '<td class=dr>'.stnum($ans['initial:'.$i]).'</td>';
-		$pg .= '<td class=dr>'.stnum($ans['allocated:'.$i]).'</td>';
+		$pg .= "<td class=dr data-srt='".$ans['allocated:'.$i]."'>".stnum($ans['allocated:'.$i]).'</td>';
 		$pg .= "<td class=dr data-srt='".$ans['instore:'.$i]."'>".stnum($ans['instore:'.$i]).'</td>';
 		$pg .= "<td class=dr data-srt='".$ans['ram:'.$i]."'>".stnum($ans['ram:'.$i]).'</td>';
 		$pg .= "<td class=dr data-srt='".$ans['ram2:'.$i]."'>".stnum($ans['ram2:'.$i]).'</td>';
-		$pg .= '<td class=dr>'.stnum($ans['cull:'.$i]).'</td>';
-		$pg .= '<td class=dr>'.stnum($ans['cull_limit:'.$i]).'</td>';
+		$pg .= "<td class=dr data-srt='".$ans['cull:'.$i]."'>".stnum($ans['cull:'.$i]).'</td>';
+		$pg .= "<td class=dr data-srt='".$ans['cull_limit:'.$i]."'>".stnum($ans['cull_limit:'.$i]).'</td>';
 		$pg .= "</tr>\n";
 	}
 	$pg .= '</tbody>';
